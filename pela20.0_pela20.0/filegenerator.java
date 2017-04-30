1ce44867518bd98b6827a44c9b89d51a7172c251
Yiluo pHoton <yiluo.li.astrophysics@gmail.com>
2017-04-30 15:34:16 +0800

    placeholder for pela20.0 vs pela20.0

---
+++ pela20.0_pela20.0/filegenerator.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class filegenerator {
+
+	public static void main (String [] args) throws IOException 
+	{
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("start.txt")));
+        for (int i = 0; i < 100; i++){
+			if (i < 10)
+			    out = new PrintWriter(new BufferedWriter(new FileWriter("pela20.0_pela20.0_00" + i + ".txt")));
+            else
+				out = new PrintWriter(new BufferedWriter(new FileWriter("pela20.0_pela20.0_0" + i + ".txt")));
+        }
+		out.close();
+	}
+}
+
