a3ed993b5486683dc6e27c07bf596c5071c4bfdf
Yiluo pHoton <yiluo.li.astrophysics@gmail.com>
2017-05-02 02:58:02 +0800

    placeholders

---
+++ pela5.0_pela60.0_30*30/filegenerator.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class filegenerator {
+
+	public static void main (String [] args) throws IOException 
+	{
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("start.txt")));
+        for (int i = 0; i < 50; i++){
+			if (i < 10)
+			    out = new PrintWriter(new BufferedWriter(new FileWriter("pela5.0_pela60.0_30*30_00" + i + ".txt")));
+            else
+				out = new PrintWriter(new BufferedWriter(new FileWriter("pela5.0_pela60.0_30*30_0" + i + ".txt")));
+        }
+		out.close();
+	}
+}
+
